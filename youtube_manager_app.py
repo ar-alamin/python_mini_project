3dbbacb0cb564a3aff5278674556b854d5dea2d9
ar-alamin <aralamin.pi@yahoo.com>
2024-06-03 14:41:17 +0600

    first commit

---
+++ youtube_manager_app.py
@@ -0,0 +1,9 @@
+while True:
+    print("\n Youtube Manager | Choose an Options")
+    print("1. List all youtube video")
+    print("2. Add a youtube video")
+    print("3. Update a youtube video Details")
+    print("4. Delete a youtube video")
+    print("4. Exit the app")
+
+    choice = input("Enter your choice")
